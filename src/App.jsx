afdcd8648dd6e76c640a8f0fdd5701d99b61faf9
Gianmarco G <giangarof98@gmail.com>
2024-07-23 16:07:56 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,34 @@ function App() {
   const [post, setPost] = useState([])
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState()
+  const [nodata, setNoData] = useState('')
 
   const access =  import.meta.env.VITE_ACCESS
   const url = `https://api.unsplash.com/search/photos?client_id=${access}&query=${term}&page=1&per_page=15`
 
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        setPost(data.results)
-        setIsLoading(false)
-      })
-      .catch(err => console.error(err))
+    const fetchData = () => {
+      try {
+        fetch(url)
+          .then(res => res.json())
+          .then(data => {
+            setPost(data.results)
+            setIsLoading(false)
+            if(data.results == ''){
+              setNoData("Your query can't retrieve any meaninful gallery, try again.")
+            } else {
+              setNoData('')
+            }
+          })
+          .catch(err => console.log(err))
+
+      } catch (error) {
+        console.error(error)
+        
+      }
+
+    }
+    fetchData()
   },[term])
  
   const style = {
@@ -33,17 +49,18 @@ function App() {
     },
     inner: {
       background:'rgba(0,0,0, .06)'
-  
     },
 
   }
   
   return (
     <>
+      
       <div style={style.header}>
         <h1>Image Gallery</h1>
         <p>With Unsplash API</p>
         <SearchTerm searchText={(text) => setTerm(text)}/>
+        {post && <p>{nodata}</p>}
       </div>
       <div style={style.outer} className='outer'>
         {post.map(p => (
@@ -55,7 +72,8 @@ function App() {
             <TextBox desc={p.alt_description} likes={p.likes} user={p.user.first_name}/>
           </div>
         ))}
-      </div>
+      </div> 
+
     </>
   )
 }
